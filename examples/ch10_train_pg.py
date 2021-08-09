22b36b1a2fda36ec0378967646050c7133cd065f
wslee <pocca2048@gmail.com>
2021-08-09 19:44:56 +0900

    add ch10 explanation

--- examples/ch10_train_pg.py
+++ examples/ch10_train_pg.py
@@ -12,6 +12,18 @@ import tensorflow as tf
 tf.compat.v1.disable_eager_execution() # 이걸 안하면 SGD가 안먹힘. keras가 tf에서인지 그냥에서인지에 따라 까다로움.
 
 def main():
+    """
+    ch 10 policy gradient learning 설명 추가:
+    여기서 사용된 건 Monte Carlo policy gradient = REINFORCE 방법.
+    1. 경험 데이터로부터 위치 (state)를 가져온다.
+    2. DNN이 다음에 둘 수를 예측 (predict action)
+    3. 에이전트가 대국에서 이기고 짐에 따라 타겟 벡터에 1이나 -1을 넣는다. (reward)
+    4. 2에서의 확률 분포와 3에서의 타깃 벡터 간의 차이가 경사를 만들고 gradient descent 한다.
+    5. gradient update가 끝나면 같은 state가 주어졌을 때 새로운 확률분포로 예측한다.
+    6. 이때 졌다면 agent가 선택한 수의 확률이 약간 줄어들 것이고 이겼으면 약간 올라갈 것이다.
+
+    REINFORCE : REward Increment = Nonnegative Factor times Offset Reinforcement times Characteristic Eligibility
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('--agent-in', required=True)
     parser.add_argument('--agent-out', required=True)
